1e23eb6bc8ff11017d98163d3461edc26e99ab18
Igor <ig1985@mail.ua>
2017-01-23 08:15:56 +0200

    second commit

--- answer23_01.php
+++ answer23_01.php
@@ -6,3 +6,20 @@
  */
 
 echo 'Hellow world';
+echo '<br>';
+
+$chanel;
+$manufactoryAddress;
+$carsColor;
+$waterTemperature;
+$telefoneModel;
+
+$x = 3;
+$y = 5;
+$z = 8;
+echo $x;
+echo '<br>';
+echo $y;
+echo '<br>';
+echo $z;
+echo '<br>';
