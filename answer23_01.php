ca620fd1e3b2bbfa7101a7198b88542d29a68284
Igor <ig1985@mail.ua>
2017-01-23 09:09:04 +0200

    third commit

--- answer23_01.php
+++ answer23_01.php
@@ -23,3 +23,55 @@ echo $y;
 echo '<br>';
 echo $z;
 echo '<br>';
+$s = $x+$y+$z;
+echo $s;
+echo '<br>';
+$result = 2+6+2/5-1;
+echo $result;
+echo '<br>';
+$a = 1;
+$b = 2;
+echo $a;
+echo '<br>';
+echo $b;
+echo '<br>';
+echo '<br>';
+$c = &$a;
+$d = &$b;
+echo $c;
+echo '<br>';
+echo $d;
+echo '<br>';
+echo '<br>';
+$a = 3;
+$b = 4;
+echo $a;
+echo '<br>';
+echo $b;
+echo '<br>';
+echo $c;
+echo '<br>';
+echo $d;
+echo '<br>';
+echo '<br>';
+define('FIRST',41);
+define('SECOND',33);
+echo FIRST+SECOND;
+echo '<br>';
+echo '<br>';
+FIRST = SECOND;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
